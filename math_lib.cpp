25d57496e416da04cf00072fe56c7a2e57f3ef03
Paraniuk-mykhailo <paraniuk.mykhailo.clg@chnu.edu.ua>
2024-09-28 17:26:39 +0300

    added file main.cpp

--- math_lib.cpp
+++ math_lib.cpp
@@ -1,1 +1,14 @@
-some content
+#include "math_lib.h"
+
+// Функція для обчислення числа Фібоначчі рекурсивно
+int fibonacci(int n) {
+    if (n <= 0) {
+        return 0; // базовий випадок
+    }
+    else if (n == 1) {
+        return 1; // базовий випадок
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2); // рекурсивний виклик
+    }
+}
--- math_lib.h
+++ math_lib.h
@@ -1,1 +1,8 @@
-some content
+
+#ifndef MATH_LIB_H
+#define MATH_LIB_H
+
+// Функція для обчислення числа Фібоначчі
+int fibonacci(int n);
+
+#endif // MATH_LIB_H
