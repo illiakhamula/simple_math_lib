75482cca3ff9deb9ada6d7dfad8b8aeafc0a0098
Paraniuk-mykhailo <118271095+Paraniuk-mykhailo@users.noreply.github.com>
2024-10-13 19:06:45 +0300

    Update math_library_test.cpp

--- math_library_test.cpp
+++ math_library_test.cpp
@@ -1,52 +1,64 @@
-#include <iostream>
 #include "math_lib.h"
+#include <gtest/gtest.h>
 
-void testFibonacci() {
-    // Тести для функції Fibonacci
-    std::cout << "Тестування функції Fibonacci:\n";
+namespace MathLibTest {
 
-    // Тест 1
-    if (fibonacci(0) == 0) {
-        std::cout << "Тест 1 пройдено: fibonacci(0) = 0\n";
-    }
-    else {
-        std::cout << "Тест 1 не пройдено: fibonacci(0) != 0\n";
+    TEST(MathLibTests, AddTest) {
+        EXPECT_EQ(MathLib::add(2, 3), 5);
+        EXPECT_EQ(MathLib::add(-2, 3), 1);
     }
 
-    // Тест 2
-    if (fibonacci(1) == 1) {
-        std::cout << "Тест 2 пройдено: fibonacci(1) = 1\n";
-    }
-    else {
-        std::cout << "Тест 2 не пройдено: fibonacci(1) != 1\n";
+    TEST(MathLibTests, SubtractTest) {
+        EXPECT_EQ(MathLib::subtract(5, 3), 2);
+        EXPECT_EQ(MathLib::subtract(3, 5), -2);
     }
 
-    // Тест 3
-    if (fibonacci(5) == 5) {
-        std::cout << "Тест 3 пройдено: fibonacci(5) = 5\n";
-    }
-    else {
-        std::cout << "Тест 3 не пройдено: fibonacci(5) != 5\n";
+    TEST(MathLibTests, MultiplyTest) {
+        EXPECT_EQ(MathLib::multiply(2, 3), 6);
+        EXPECT_EQ(MathLib::multiply(-2, 3), -6);
     }
 
-    // Тест 4
-    if (fibonacci(10) == 55) {
-        std::cout << "Тест 4 пройдено: fibonacci(10) = 55\n";
-    }
-    else {
-        std::cout << "Тест 4 не пройдено: fibonacci(10) != 55\n";
+    TEST(MathLibTests, DivideTest) {
+        EXPECT_EQ(MathLib::divide(6, 3), 2);
+        EXPECT_THROW(MathLib::divide(6, 0), std::invalid_argument);
     }
 
-    // Тест 5
-    if (fibonacci(15) == 610) {
-        std::cout << "Тест 5 пройдено: fibonacci(15) = 610\n";
+    TEST(MathLibTests, IsEqualTest) {
+        EXPECT_TRUE(MathLib::isEqual(0.1 + 0.2, 0.3));
+        EXPECT_FALSE(MathLib::isEqual(1.0, 2.0));
     }
-    else {
-        std::cout << "Тест 5 не пройдено: fibonacci(15) != 610\n";
+
+    TEST(MathLibTests, SqrtTest) {
+        EXPECT_EQ(MathLib::sqrt(4), 2);
+        EXPECT_THROW(MathLib::sqrt(-1), std::invalid_argument);
+    }
+
+    TEST(MathLibTests, IsPrimeTest) {
+        EXPECT_TRUE(MathLib::isPrime(5));
+        EXPECT_FALSE(MathLib::isPrime(4));
+        EXPECT_FALSE(MathLib::isPrime(1));
+    }
+
+    TEST(MathLibTests, LCMTest) {
+        EXPECT_EQ(MathLib::leastCommonMultiple(4, 6), 12);
+        EXPECT_EQ(MathLib::leastCommonMultiple(3, 5), 15);
+    }
+
+    TEST(MathLibTests, GCDTest) {
+        EXPECT_EQ(MathLib::GCD(12, 15), 3);
+        EXPECT_EQ(MathLib::GCD(17, 19), 1);
+    }
+
+    TEST(MathLibTests, FibonacciTest) {
+        EXPECT_EQ(MathLib::fibonacci(0), 0);
+        EXPECT_EQ(MathLib::fibonacci(1), 1);
+        EXPECT_EQ(MathLib::fibonacci(5), 5);
+        EXPECT_EQ(MathLib::fibonacci(10), 55);
+        EXPECT_THROW(MathLib::fibonacci(-1), std::invalid_argument);
     }
 }
 
-int main() {
-    testFibonacci();
-    return 0;
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
